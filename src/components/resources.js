4b67561ebc30a3051fa7a992a78928db5aea6cf2
Mario de Biase <debiasej@gmail.com>
2017-02-12 14:44:33 +0100

    Add resources page and its contents

---
+++ src/components/resources.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default () => {
+  return(
+    <div>
+      The NOC List
+      <div>
+        <img src="../../img/disk.jpeg" />
+        <ul className="list">
+          <li>Tom Cruise</li>
+          <li>Emmanuelle Béart</li>
+          <li>Jean Reno</li>
+          <li>Ving Rhames</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
